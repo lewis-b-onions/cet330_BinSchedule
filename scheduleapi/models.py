aca5804f190fcec7ade9e6b35f40fd7191bb2fd0
lewis-b-onions <lewisbonions@gmail.com>
2019-01-15 17:16:01 +0000

    Added new council reg app

--- scheduleapi/models.py
+++ scheduleapi/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
-
-
 class CouncilRegistration(models.Model):
     council_id = models.AutoField(primary_key=True)
     council_name = models.CharField(max_length=60)
